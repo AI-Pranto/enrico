2afc5ed248516cb50684f648f4f6f1077b2e5f97
Patrick Shriwise <pshriwise@gmail.com>
2020-10-05 16:32:11 -0500

    List dir.

--- ci/test_singlerod_openmc_openfoam.sh
+++ ci/test_singlerod_openmc_openfoam.sh
@@ -6,6 +6,7 @@ curdir="$(pwd)"
 cp -r ./config ~/.OpenFOAM
 cd tests/singlerod/short/openmc_openfoam
 WM_PROJECT_DIR=$curdir/vendor/OpenFOAM-dev/ ./Allmesh
+ls ../build
 mpirun -np 2 ../build/enrico
 
 
