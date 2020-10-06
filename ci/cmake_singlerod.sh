4751342d88138a24f5e495494cb4f3cc3b2c14d1
Patrick Shriwise <pshriwise@gmail.com>
2020-10-06 09:47:40 -0500

    Trying mpicxx instead of mpic++.

--- ci/cmake_singlerod.sh
+++ ci/cmake_singlerod.sh
@@ -5,5 +5,5 @@ mkdir -p tests/singlerod/short/build
 cd tests/singlerod/short/build
 echo $(which mpicxx)
 echo $(cmake --version)
-CC=$(which mpicc) CXX=$(which mpic++) FC=$(which mpif90) \
+CC=$(which mpicc) CXX=$(which mpicxx) FC=$(which mpif90) \
   cmake -DUSE_FOAM=ON -DUSR_LOC=../ ../../../..
