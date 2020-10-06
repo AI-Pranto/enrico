44f9fdefd5e5e7cd798953dd2cf7363de8b5ad85
Patrick Shriwise <pshriwise@gmail.com>
2020-10-05 22:47:02 -0500

    Trying echo of cmake version.

--- ci/cmake_singlerod.sh
+++ ci/cmake_singlerod.sh
@@ -3,5 +3,6 @@ set -ex
 
 mkdir -p tests/singlerod/short/build
 cd tests/singlerod/short/build
+echo $(cmake --version)
 CC=mpicc CXX=mpicxx FC=mpif90 \
   cmake -DUSE_FOAM=ON -DUSR_LOC=../ ../../../..
