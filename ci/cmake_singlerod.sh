6d4afe0ecc8ee48f5cca8a1d3ed226320b7f1d93
Patrick Shriwise <pshriwise@gmail.com>
2020-10-07 16:29:40 -0500

    Adding CMake compiler defs back in.

--- ci/cmake_singlerod.sh
+++ ci/cmake_singlerod.sh
@@ -5,6 +5,6 @@ mkdir -p tests/singlerod/short/build
 cd tests/singlerod/short/build
 echo $(which mpicxx)
 echo $(cmake --version)
-echo $(/usr/bin/mpic++ -show)
-CC=$(which mpicc) CXX=$(which mpic++) FC=$(which mpif90) \
-  cmake -DUSE_FOAM=ON -DUSR_LOC=../ ../../../..
+echo $(/usr/bin/mpicxx -show)
+CC=$(which mpicc) CXX=$(which mpicxx) FC=$(which mpif90) \
+  cmake -DCMAKE_CXX_COMPILER=/usr/bin/mpicxx -DCMAKE_C_COMPILER=/usr/bin/mpicc -DCMAKE_Fortran_COMPILER=/usr/bin/mpifort -DUSE_FOAM=ON -DUSR_LOC=../ ../../../..
