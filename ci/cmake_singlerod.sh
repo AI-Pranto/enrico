dbd916a15f354505add6b28e72ba20a5f23a5110
AI-Pranto <pranto.roza.53@gmail.com>
2020-12-10 23:30:38 +0600

    Add flex

--- ci/cmake_singlerod.sh
+++ ci/cmake_singlerod.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -ex
-
+pip install cmake
+echo 'export PATH=$PATH:~/.local/bin' >> ~/.bashrc
 mkdir -p tests/singlerod/short/build
 cd tests/singlerod/short/build
 echo $(which mpicxx)
 echo $(cmake --version)
 echo $(/usr/bin/mpicxx -show)
 CC=$(which mpicc) CXX=$(which mpicxx) FC=$(which mpif90) \
-  cmake -DCMAKE_CXX_COMPILER=/usr/bin/mpicxx -DCMAKE_C_COMPILER=/usr/bin/mpicc -DCMAKE_Fortran_COMPILER=/usr/bin/mpifort -DUSE_FOAM=ON -DBUILD_FOAM=ON -DUSE_NEK=OFF ../../../..
+       	cmake -DCMAKE_CXX_COMPILER=/usr/bin/mpicxx -DCMAKE_C_COMPILER=/usr/bin/mpicc \
+	-DCMAKE_Fortran_COMPILER=/usr/bin/mpifort -DUSE_FOAM=ON -DBUILD_FOAM=ON -DUSE_NEK=OFF ../../../..
