331ba79cb3206bffd4ec1632cc8602994565cd88
AI-Pranto <pranto.roza.53@gmail.com>
2020-12-10 17:27:35 +0600

    Build Nek off

--- ci/cmake_singlerod.sh
+++ ci/cmake_singlerod.sh
@@ -7,4 +7,4 @@ echo $(which mpicxx)
 echo $(cmake --version)
 echo $(/usr/bin/mpicxx -show)
 CC=$(which mpicc) CXX=$(which mpicxx) FC=$(which mpif90) \
-  cmake -DCMAKE_CXX_COMPILER=/usr/bin/mpicxx -DCMAKE_C_COMPILER=/usr/bin/mpicc -DCMAKE_Fortran_COMPILER=/usr/bin/mpifort -DUSE_FOAM=ON -DBUILD_FOAM=ON -DUSR_LOC=../ ../../../..
+  cmake -DCMAKE_CXX_COMPILER=/usr/bin/mpicxx -DCMAKE_C_COMPILER=/usr/bin/mpicc -DCMAKE_Fortran_COMPILER=/usr/bin/mpifort -DUSE_FOAM=ON -DBUILD_FOAM=ON -DUSE_NEK=OFF ../../../..
